7e2332181fe3114f5c40d6fcaea26b0639ba8834
WahidaAkhter <wahidacse280@gmail.com>
2022-04-16 23:21:42 +0600

    header part done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Notfound from './Components/NotFound/Notfound';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      
+      <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
      
     </div>
   );
--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Container>
+        <Navbar.Brand className='text-dark' href="#home">Weattle...</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className='text-dark' href="#services">Services</Nav.Link>
+            <Nav.Link className='text-dark' href="#about me">About Me</Nav.Link>
+            </Nav>
+          <Nav>
+            <Nav.Link className='text-dark' href="#deets">Login</Nav.Link>
+            <Nav.Link className='text-dark'  href="#memes">
+              Sign In
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
