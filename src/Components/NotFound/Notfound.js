e809f6f02dabf291b07339845840072535b2a650
WahidaAkhter <wahidacse280@gmail.com>
2022-04-16 23:22:24 +0600

    not found part done

--- src/Components/NotFound/Notfound.js
+++ src/Components/NotFound/Notfound.js
@@ -5,7 +5,7 @@ const Notfound = () => {
         <div className='position-absolute top-50 start-50 translate-middle'>
             <img src="https://cdn-icons-png.flaticon.com/512/763/763707.png?w=740" alt="" />
             <h1 className='text-danger'>404!!!</h1>
-            <h2> <span className='text-warning'> Sorry</span> your developer is sleeping.........</h2>
+            <h2> <span className='text-warning'> Sorry</span> your developer is sleeping........</h2>
         </div>
     );
 };
