041701d0cd205ac8d292f0ad2a7b522a148cdb79
WahidaAkhter <wahidacse280@gmail.com>
2022-04-17 14:19:38 +0600

    firebase installed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './Components/About/About';
 import Blogs from './Components/Blogs/Blogs';
 import ServiceDetail from './Components/ServiceDetail/ServiceDetail';
 import Login from './Components/Login/Login/Login';
+import Register from './Components/Login/Register/Register';
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
 
         <Route path="/login" element={<Login></Login>}></Route>
+
+        <Route path="/register" element={<Register></Register>}></Route>
+
 
         <Route path="/about" element={<About></About>}></Route>
 
--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import Header from '../../Home/Header/Header';
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
     return (
         <div>
-            <h2>please register</h2>
+            <Header></Header>
+            <h2 className='text-primary text-center mt-5 mb-5'>Please Register</h2>
+            <div className='container w-50 mx-auto'>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <p>Already Registerd? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+            </div>
         </div>
     );
 };
